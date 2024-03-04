112a4c068505ff5f3d16a04b61000bcbe07f74f5
Yasuaki Suto <sutou326@yahoo.co.jp>
2024-03-04 19:08:52 +0900

    ImageMessage追加

--- receipt_ocr.py
+++ receipt_ocr.py
@@ -16,7 +16,7 @@ from flask import Flask, request, abort
 
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageMessage
 
 class FeatureType(Enum):
     PAGE = 1
