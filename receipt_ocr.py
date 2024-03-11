c075a2bc62c5e0eddfd1c4effea93479902b46cd
Yasuaki Suto <sutou326@yahoo.co.jp>
2024-03-11 22:41:36 +0900

    threshold調整

--- receipt_ocr.py
+++ receipt_ocr.py
@@ -109,13 +109,14 @@ def get_sorted_lines(response):
     char_height = -1
     for bound in bounds:
       #print('bound')
-      #print(bound)
+      print(bound)
+      print("old_y=%d" % old_y)
       x = bound[0]
       #y = bound[1]
       y = bound[3].vertices[0].y + (bound[3].vertices[2].y - bound[3].vertices[0].y) / 2 #文字の中央にする
       if char_height == -1:
           char_height = bound[3].vertices[2].y - bound[3].vertices[0].y
-      threshold = int(char_height * 0.4)
+      threshold = int(char_height * 0.3)
       #print("threshold=%d" % threshold)
       
       if old_y == -1:
